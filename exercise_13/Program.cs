6b0da8fc2aafa72a62692699c1761fd944986a47
will <wschoenle93@gmail.com>
2021-04-14 13:43:13 -0400

    first commit for exercis 13

---
+++ exercise_13/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace exercise_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool repeat = true;
+            string userResponse = "";
+
+            while (repeat)
+            {
+                Console.WriteLine("Enter a number:");
+                string userInput = Console.ReadLine();
+                int userNumber = int.Parse(userInput);
+
+                for (int i = userNumber; i >= 0; i--)
+                {
+                    Console.WriteLine(i);
+
+                }
+
+                Console.WriteLine("Would you like to continue? (y|n)");
+                userResponse = Console.ReadLine();
+
+                if (userResponse == "y")
+                {
+                    repeat = true;
+                }
+                else if (userResponse == "n")
+                {
+                    repeat = false;
+                }
+
+            }
+
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
